8b3694fd476a509c0d272fef1289ee432b3c4427
fawaswali <fawaswali@gmail.com>
2026-02-20 15:36:39 +0700

    update

--- P2/MahasiswaMain07.java
+++ P2/MahasiswaMain07.java
@@ -6,12 +6,16 @@ public class MahasiswaMain07 {
         mhsl.nama = "Muhammad Ali Farhan";
         mhsl.nim = "2241720171";
         mhsl.kelas = "SI 2J";
-        mhsl.ipk = 4.55;
+        mhsl.ipk = 3.55;
 
         mhsl.tampilkaninformasi();
         mhsl.ubahKelas("SI 2K");
-        mhsl.updateIpk(4.60);
+        mhsl.updateIpk(3.60);
         mhsl.tampilkaninformasi();
+
+        Mahasiswa07 mhs2 = new Mahasiswa07("Annisa Nabila","2141720160",3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkaninformasi();
 
     }
 }
