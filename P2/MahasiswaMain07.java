9c425510acb986f5410e478493a5ad2c15c8b733
fawaswali <fawaswali@gmail.com>
2026-02-23 07:44:30 +0700

    P2_Percobaan 3

--- P2/MahasiswaMain07.java
+++ P2/MahasiswaMain07.java
@@ -6,16 +6,18 @@ public class MahasiswaMain07 {
         mhsl.nama = "Muhammad Ali Farhan";
         mhsl.nim = "2241720171";
         mhsl.kelas = "SI 2J";
-        mhsl.ipk = 3.55;
+        mhsl.ipk = 4.55;
 
         mhsl.tampilkaninformasi();
         mhsl.ubahKelas("SI 2K");
-        mhsl.updateIpk(3.60);
+        mhsl.updateIpk(4.60);
         mhsl.tampilkaninformasi();
 
-        Mahasiswa07 mhs2 = new Mahasiswa07("Annisa Nabila","2141720160",3.25,"TI 2L");
+        Mahasiswa07 mhs2 = new Mahasiswa07("Annisa Nabila", "2141720172", 3.85, "TI 2L");
         mhs2.updateIpk(3.30);
         mhs2.tampilkaninformasi();
 
+        Mahasiswa07 mhsFawas = new Mahasiswa07("Fawas Saqif Abdullohil Wali", "254107020037", 3.80, "TI 1E");
+        mhsFawas.tampilkaninformasi();
     }
 }
