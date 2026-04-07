77b469527ec29a338917b6ec24b218a32e3039a6
fawaswali <fawaswali@gmail.com>
2026-04-07 21:58:57 +0700

    Praktikum5_P2_a_pertanyaan2

--- Praktikum05/MahasiswaDemo07.java
+++ Praktikum05/MahasiswaDemo07.java
@@ -1,25 +1,38 @@
 package Praktikum05;
 
+import java.util.Scanner;
+
 public class MahasiswaDemo07 {
     public static void main(String[] args) {
+        Scanner fawas = new Scanner(System.in);
         MahasiswaBerprestasi07 list = new MahasiswaBerprestasi07();
-        Mahasiswa07 m1 = new Mahasiswa07("001", "Andi", "TI-1A", 3.5);
-        Mahasiswa07 m2 = new Mahasiswa07("002", "Budi", "TI-1A", 3.8);
-        Mahasiswa07 m3 = new Mahasiswa07("003", "Citra", "TI-1B", 3.2);
-        Mahasiswa07 m4 = new Mahasiswa07("004", "Dina", "TI-1B", 3.9);
-        Mahasiswa07 m5 = new Mahasiswa07("005", "Eko", "TI-1C", 3.4);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i+1));
 
-        System.out.println("Data mahasiswa sebelum sorting");
+            System.out.print("NIM: ");
+            String nim = fawas.nextLine();
+
+            System.out.print("Nama: ");
+            String nama = fawas.nextLine();
+
+            System.out.print("Kelas: ");
+            String kelas = fawas.nextLine();
+
+            System.out.print("IPK: ");
+            double ipk = fawas.nextDouble();
+            fawas.nextLine(); 
+
+            Mahasiswa07 m = new Mahasiswa07(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("Data sebelum sorting:");
         list.tampil();
 
-        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
         list.bubbleSort();
+
+        System.out.println("Data setelah sorting (DESC):");
         list.tampil();
     }
 }
